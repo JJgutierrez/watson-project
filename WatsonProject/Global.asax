e36a6eb16b901fd4df6db19061288a0c0af432ea
JJgutierrez <jjgues@live.com>
2016-04-22 15:27:47 -0500

    initail commit

---
+++ WatsonProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WatsonProject.MvcApplication" Language="C#" %>
---
+++ WatsonProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WatsonProject.Startup))]
+namespace WatsonProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
