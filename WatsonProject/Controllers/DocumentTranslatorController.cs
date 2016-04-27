3650476d224e9997d097b205fc31d5dc963a8fa3
JJgutierrez <jjgues@live.com>
2016-04-27 10:05:20 -0500

    some view added

---
+++ WatsonProject/Controllers/DocumentTranslatorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WatsonProject.Controllers
+{
+    public class DocumentTranslatorController : Controller
+    {
+        // GET: DocumentTranslator
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WatsonProject/Controllers/RealTimeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WatsonProject.Controllers
+{
+    public class RealTimeController : Controller
+    {
+        // GET: RealTime
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
