e273350b7f366d85fcf28a59b02c8315a9839b0b
JJgutierrez <jjgues@live.com>
2016-05-04 15:12:13 -0500

    testing

--- WatsonProject/Controllers/HomeController.cs
+++ WatsonProject/Controllers/HomeController.cs
@@ -3,11 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using WatsonProject.Models;
 
 namespace WatsonProject.Controllers
 {
+    //[Authorize]
     public class HomeController : Controller
     {
+        //private ApplicationSignInManager _signInManager;
+        //private ApplicationUserManager _userManager;
+
+        //public HomeController()
+        //{
+        //}
+
+        //public HomeController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
+        //{
+        //    UserManager = userManager;
+        //    SignInManager = signInManager;
+        //}
+
+        //public ApplicationSignInManager SignInManager
+        //{
+        //    get
+        //    {
+        //        return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+        //    }
+        //    private set
+        //    {
+        //        _signInManager = value;
+        //    }
+        //}
         public ActionResult Index()
         {
             return View();
